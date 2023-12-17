9234fe3ffb627276bab2e0fe6a67001b0d8b41af
Brandon Trecki <treckstar@gmail.com>
2023-12-16 23:53:13 -0500

    fix: default env example variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,9 +8,9 @@ export default defineNuxtConfig({
   uniform: {
     projectId: process.env.UNIFORM_PROJECT_ID,
     readOnlyApiKey: process.env.UNIFORM_API_KEY,
-    apiHost: process.env.UNIFORM_CLI_BASE_URL,
-    edgeApiHost: process.env.UNIFORM_CLI_BASE_EDGE_URL,
-    outputType: process.env.OUTPUT_TYPE,
+    apiHost: process.env.UNIFORM_CLI_BASE_URL || "https://uniform.app",
+    edgeApiHost: process.env.UNIFORM_CLI_BASE_EDGE_URL || "https://uniform.global",
+    outputType: process.env.OUTPUT_TYPE || 'standard',
     manifest: manifestJson as ManifestV2,
     defaultConsent: true,
     // If you prefer to have full control over your Uniform Context instance:
